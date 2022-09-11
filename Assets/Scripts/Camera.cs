c8182d75ac64417e002a8ec4797699185b86b890
web3ten0 <web3ten0@gmail.com>
2022-09-12 00:38:53 +0900

    camera追従

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject cam;
+    private GameObject player;
+    private Vector3 player_position;
+    public float camera_y = 3.0f;
+    public float camera_z = -12.0f;
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        player_position = player.transform.position;
+        player_position.y += camera_y;
+        player_position.z += camera_z;
+        cam.transform.position = player_position;
+    }
+}
