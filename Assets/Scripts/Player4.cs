6c2493814f5ff908b7f73a397cc450b2f137445b
web3ten0 <web3ten0@gmail.com>
2022-09-17 22:04:06 +0900

    接地判定, walk_speed, is_groundedをanimatorにセット

--- Assets/Scripts/Player4.cs
+++ Assets/Scripts/Player4.cs
@@ -3,17 +3,23 @@ using UnityEngine.InputSystem;
 
 public class Player4 : MonoBehaviour
 {
+    public float default_speed = 5.0f;
+    public float ground_radius = 3.0f;
+    public LayerMask ground_layers;
+
     private CharacterController _characon;
     private PlayerInput _input;
+    private Animator _animator;
     Vector3 direction;
-    float speed = 5.0f;
-    bool is_walking = false;
+
     bool is_running = false;
+    bool is_grounded = false;
 
     private void Awake()
     {
         TryGetComponent(out _characon);
         TryGetComponent(out _input);
+        TryGetComponent(out _animator);
     }
 
     private void OnEnable()
@@ -36,7 +42,7 @@ public class Player4 : MonoBehaviour
     {
         var value = obj.ReadValue<Vector2>();
         direction = new Vector3(value.x, 0, value.y);
-        is_walking = value.x != 0 && value.y != 0;
+        Debug.Log("direction: " + direction);
     }
 
     private void OnDash4(InputAction.CallbackContext obj)
@@ -44,14 +50,23 @@ public class Player4 : MonoBehaviour
         is_running = obj.phase == InputActionPhase.Started ? true : false;
     }
 
-    void Start()
+    private void grounded_check()
     {
+        is_grounded = Physics.CheckSphere(transform.position, ground_radius, ground_layers, QueryTriggerInteraction.Ignore);
+        _animator.SetBool("is_grounded", is_grounded);
+    }
 
+    private void move()
+    {
+        var _speed = is_running ? default_speed * 2.0f : default_speed;
+        var distance = direction * _speed * Time.deltaTime;
+        _characon.Move(distance);
+        _animator.SetFloat("walk_speed", distance.magnitude);
     }
 
     void Update()
     {
-        var _speed = is_running ? speed * 2.0f : speed;
-        _characon.Move(direction * _speed * Time.deltaTime);
+        grounded_check();
+        move();
     }
 }
